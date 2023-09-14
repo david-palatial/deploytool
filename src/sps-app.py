39db8103b03c5f36cd479290334221779a8605ea
David Godfrey <david@palatialxr.com>
2023-09-14 11:57:19 -0400

    update

--- src/sps-app.py
+++ src/sps-app.py
@@ -420,9 +420,11 @@ elif command == "setup":
     help_menus.show_setup_help()
     sys.exit(0)
 
+  print(env_path)
   if not os.path.exists(env_path):
     subprocess.run(f'scp {misc.host}:/mnt/unreal-project/.env {exe_path}')
     env_values = dotenv_values(env_path)
+  print(os.path.exists(env_path))
 
   if len(sys.argv) == 3 and sys.argv[2] == "--stdin":
     server = input(f"Server name [{env_values['SPS_REST_API_SERVER']}]: ")

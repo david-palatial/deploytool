36bc5514a5a316c46202b5d82262984d77e315e7
David Godfrey <david@palatialxr.com>
2023-09-14 11:39:31 -0400

    update

--- src/misc.py
+++ src/misc.py
@@ -29,7 +29,7 @@ def get_exe_directory():
 exe_path = get_exe_directory()
 env_values = dotenv_values(os.path.join(exe_path, ".env"))
 print('helllo')
-host = env_values.get('HOST', 'david@palatial.tenant-palatial-platform.coreweave.cloud')
+#host = env_values.get('HOST', 'david@palatial.tenant-palatial-platform.coreweave.cloud')
 
 def file_exists_on_remote(host, remote_file_path):
     try:

7f3e0e79f020d01ce254eae2a55920c680aabdc5
David Godfrey <david@palatialxr.com>
2023-11-29 13:17:46 -0500

    update

--- src/sps-app.py
+++ src/sps-app.py
@@ -41,7 +41,7 @@ def GetKey():
   ps_code = r"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secrets/sps-api-access-key --template='{{.data.restapiaccesskey}}')))"
 
   # Execute PowerShell code using subprocess
-  result = subprocess.run(['powershell', '-Command', ps_code], stdout=subprocess.PIPE)
+  result = subprocess.run(['powershell', '-Command', ps_code], capture_output=True, Text=True)
 
   # Check the result
   if result.returncode == 0:

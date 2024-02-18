376cc6fa5d0189c5e276b1faecc2a323a3f1dc7b
David Godfrey <david@palatialxr.com>
2024-02-18 18:36:51 -0500

    update

--- src/sps-app.py
+++ src/sps-app.py
@@ -44,8 +44,9 @@ def install_kubectl():
   elif platform.system() == "Windows":
     print("on windows")
     kube_folder = os.path.join(os.environ.get("USERPROFILE"), ".kube")
+    k8s_script = os.path.join(exe_path, "dist", "k8ctl_setup.ps1")
     ps_script = "$([scriptblock]::Create((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/coreweave/kubernetes-cloud/master/getting-started/k8ctl_setup.ps1')))"
-    process = subprocess.Popen(["start", "powershell", '-Command', ps_script], shell=True)
+    process = subprocess.Popen(["start", "powershell", "-NoExit", "-ExecutionPolicy", "ByPass", "-File", k8s_script], shell=True)
     process.wait()
     print("downloaded")
     print("making directory")
